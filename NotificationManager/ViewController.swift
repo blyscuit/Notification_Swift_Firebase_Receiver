531675308025b00b72054b9484558cac5eeda746
Bliss <pisit.w@23perspective.com>
2020-01-20 12:42:11 +0700

    Initial Commit

---
+++ NotificationManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationManager
+//
+//  Created by Pisit W on 20/1/2563 BE.
+//  Copyright © 2563 23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NotificationManagerTests/NotificationManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationManagerTests.swift
+//  NotificationManagerTests
+//
+//  Created by Pisit W on 20/1/2563 BE.
+//  Copyright © 2563 23. All rights reserved.
+//
+
+import XCTest
+@testable import NotificationManager
+
+class NotificationManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
